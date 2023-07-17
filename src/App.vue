b453583f7862df344f49327b3c5a22eb0b1368e6
Lorenzo Favaretto <114069116+lorenz2396@users.noreply.github.com>
2023-07-17 17:40:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div>
+    APP
+  </div>
+</template>
+
+<style lang="scss">
+@use "assets/scss/main";
+</style>
